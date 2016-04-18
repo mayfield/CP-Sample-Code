f6b9780f937bfe57017b61aee2f656327cc714fa
Lynn August Linse <lynn0git@linse.org>
2016-04-17 21:32:57 -0500

    remove some other

--- RouterSDKDemo/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "INSTALLATION for RouterSDKDemo on:" >> install.log
-date >> install.log
--- py35_flynroot.bat
+++
@@ -1,5 +0,0 @@
-set PATH=%PATH%;C:\Python35
-
-f:
-cd \lynn\root
-set PYTHONPATH=F:\lynn\root;
